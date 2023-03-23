fd74ca62379ac71e48d159b826d25840cf577796
White Beef <whitebeef.official@gmail.com>
2023-03-23 03:39:09 +0300

    Added first realisation of InventoryGUI

---
+++ src/main/java/ru/whitebeef/beeflibrary/inventory/IInventoryGUI.java
@@ -0,0 +1,153 @@
+package ru.whitebeef.beeflibrary.inventory;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiPredicate;
+import java.util.stream.IntStream;
+
+public interface IInventoryGUI extends ClickableInventory, RenameableInventory {
+
+    Builder builder(String namespace, int size);
+
+    @NotNull String getNamespace();
+
+    @NotNull Component getName();
+
+    int getSize();
+
+    void setItem(int slot, @Nullable ItemStack item);
+
+    boolean testPredicate(@NotNull Player player, @NotNull ItemStack item, int slot);
+
+    @Nullable ItemStack getItem(int slot);
+
+    HashMap<Integer, ItemStack> addItem(@Nullable ItemStack itemStack);
+
+    void open(@NotNull Player player);
+
+    @NotNull Set<@NotNull Player> getOpenedPlayers();
+
+    void setStorageContents(@Nullable ItemStack @NotNull [] items);
+
+    List<String> getCommands(int slot);
+
+    @Nullable ItemStack @NotNull [] getStorageContents();
+
+    void close(Player player);
+
+    void close();
+
+    @NotNull Set<Integer> getClosedSlots();
+
+    boolean isSlotClosed(int slot);
+
+    @NotNull List<@NotNull String> getCommandsOnClose();
+
+    void closeSlot(int slot);
+
+    void openSlot(int slot);
+
+    void setCommandsOnClose(@NotNull List<@NotNull String> commands);
+
+    @NotNull Inventory getInventory();
+
+    abstract class Builder {
+
+        protected Set<Integer> closedSlots = new HashSet<>();
+        protected List<String> commandsOnClose = new ArrayList<>();
+        protected String namespace;
+        protected int size;
+        protected Component name;
+        protected Map<Integer, @NotNull BiPredicate<@NotNull Player, @Nullable ItemStack>> predicates;
+        protected Map<Integer, List<String>> commands;
+        protected ItemStack[] items;
+
+
+        public Builder(@NotNull String namespace, int size) {
+            this.namespace = namespace;
+            if (size < 0) {
+                throw new IllegalArgumentException("Size can't be negative! (" + size + ")");
+            }
+            if (size % 9 != 0) {
+                size += 9 - size % 9;
+            }
+            if (size >= 54) {
+                size = 54;
+            }
+            this.size = size;
+        }
+
+        public Builder setItem(int slot, @NotNull ItemStack item) {
+            if (slot < 0 || slot >= size) {
+                throw new IllegalArgumentException("Slot " + slot + " is not in range [0.." + (size - 1) + "]");
+            }
+            items[slot] = item;
+            return this;
+        }
+
+        public Builder setName(@NotNull Component name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setPredicate(int slot, @NotNull BiPredicate<@NotNull Player, @Nullable ItemStack> predicate) {
+            this.predicates.put(slot, predicate);
+            return this;
+        }
+
+        public Builder setStorageContents(@Nullable ItemStack @NotNull [] items) {
+            if (this.size != items.length) {
+                throw new IllegalArgumentException("Size of items array (" + items.length + ") not equals InventoryGUI size(" + this.size + ") ");
+            }
+            this.items = items;
+            return this;
+        }
+
+        public Builder setCommand(int slot, @NotNull String command) {
+            commands.computeIfAbsent(slot, k -> new ArrayList<>()).add(command);
+            return this;
+        }
+
+        public Builder closeSlots(int... slots) {
+            for (int slot : slots) {
+                closedSlots.add(slot);
+            }
+            return this;
+        }
+
+        public Builder openSlots(int... slots) {
+            for (int slot : slots) {
+                closedSlots.remove(slot);
+            }
+            return this;
+        }
+
+        public Builder closeAllSlots() {
+            IntStream.range(0, size - 1).forEach(closedSlots::add);
+            return this;
+        }
+
+        public Builder openAllSlots() {
+            closedSlots.clear();
+            return this;
+        }
+
+        public Builder addCloseCommands(String... command) {
+            commandsOnClose.addAll(List.of(command));
+            return this;
+        }
+
+        public abstract IInventoryGUI build();
+    }
+}
---
+++ src/main/java/ru/whitebeef/beeflibrary/inventory/impl/InventoryGUI.java
@@ -0,0 +1,194 @@
+package ru.whitebeef.beeflibrary.inventory.impl;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import ru.whitebeef.beeflibrary.inventory.IInventoryGUI;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
+
+public class InventoryGUI implements IInventoryGUI {
+
+    private final Inventory inventory;
+    private final String namespace;
+    private final int size;
+    private Component name;
+    private final Map<Integer, @NotNull BiPredicate<@NotNull Player, @Nullable ItemStack>> predicates;
+    private final Map<Integer, List<String>> commands;
+    private List<String> commandsOnClose;
+
+    private Set<Integer> closedSlots;
+
+    @Override
+    public void onClick(int slot, InventoryClickEvent event) {
+
+    }
+
+    @Override
+    public void onDrag(int slot, InventoryDragEvent event) {
+
+    }
+
+    @Override
+    public Builder builder(String namespace, int size) {
+        return new Builder(namespace, size);
+    }
+
+    public InventoryGUI(@NotNull String namespace, int size, @NotNull Component name, @NotNull Map<Integer,
+            @NotNull BiPredicate<@NotNull Player, @Nullable ItemStack>> predicates,
+                        @NotNull Map<Integer, List<@NotNull String>> commands, @NotNull ItemStack[] @NotNull items,
+                        @NotNull Set<@NotNull Integer> closedSlots, @NotNull List<@NotNull String> commandsOnClose) {
+        this.namespace = namespace;
+        this.size = size;
+        this.name = name;
+        this.predicates = predicates;
+        this.commands = commands;
+        this.closedSlots = closedSlots;
+        this.commandsOnClose = commandsOnClose;
+        inventory = Bukkit.createInventory(null, size, name);
+    }
+
+    @Override
+    @NotNull
+    public String getNamespace() {
+        return namespace;
+    }
+
+    @Override
+    @NotNull
+    public Component getName() {
+        return name;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+
+    @Override
+    public void setItem(int slot, @Nullable ItemStack item) {
+        inventory.setItem(slot, item);
+        getOpenedPlayers().forEach(Player::updateInventory);
+    }
+
+    @Override
+    public boolean testPredicate(@NotNull Player player, @NotNull ItemStack item, int slot) {
+        return predicates.getOrDefault(slot, (a, b) -> true).test(player, item);
+    }
+
+    @Override
+    @Nullable
+    public ItemStack getItem(int slot) {
+        return inventory.getItem(slot);
+    }
+
+    @Override
+    public HashMap<Integer, ItemStack> addItem(@Nullable ItemStack itemStack) {
+        //TODO сделать проверку на предикаты и добавлять только если всё проходит и слот открыт
+        return inventory.addItem(itemStack);
+    }
+
+    @Override
+    public void open(@NotNull Player player) {
+        player.openInventory(inventory);
+    }
+
+    @Override
+    @NotNull
+    public Set<@NotNull Player> getOpenedPlayers() {
+        return inventory.getViewers().stream().map(humanEntity -> (Player) humanEntity).collect(Collectors.toSet());
+    }
+
+    @Override
+    public void setStorageContents(ItemStack @NotNull [] items) {
+        inventory.setStorageContents(items);
+        getOpenedPlayers().forEach(Player::updateInventory);
+    }
+
+    @Override
+    public List<String> getCommands(int slot) {
+        return commands.getOrDefault(slot, new ArrayList<>());
+    }
+
+    @Override
+    @Nullable
+    public ItemStack @NotNull [] getStorageContents() {
+        return inventory.getStorageContents();
+    }
+
+    @Override
+    @NotNull
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    @Override
+    public void close(Player player) {
+        player.closeInventory();
+    }
+
+    @Override
+    public void close() {
+        getOpenedPlayers().forEach(this::close);
+    }
+
+    @Override
+    public @NotNull Set<Integer> getClosedSlots() {
+        return closedSlots;
+    }
+
+    @Override
+    public boolean isSlotClosed(int slot) {
+        return closedSlots.contains(slot);
+    }
+
+    @Override
+    public @NotNull List<@NotNull String> getCommandsOnClose() {
+        return commandsOnClose;
+    }
+
+    @Override
+    public void closeSlot(int slot) {
+        closedSlots.add(slot);
+    }
+
+    @Override
+    public void openSlot(int slot) {
+        closedSlots.remove(slot);
+    }
+
+    @Override
+    public void setCommandsOnClose(@NotNull List<@NotNull String> commands) {
+
+    }
+
+    @Override
+    public void rename(@NotNull Component newName) {
+
+    }
+
+    private static class Builder extends IInventoryGUI.Builder {
+
+        public Builder(String namespace, int size) {
+            super(namespace, size);
+        }
+
+        @Override
+        public IInventoryGUI build() {
+            return new InventoryGUI(namespace, size, name, predicates, commands, items, closedSlots, commandsOnClose);
+        }
+    }
+}
